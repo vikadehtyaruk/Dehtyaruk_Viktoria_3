03000f15562f40e336a8f8cdb8034313fce63772
vikadehtyaruk <vika.dehtyaruk@gmail.com>
2022-06-14 16:53:04 +0300

    add project 3

---
+++ ConsoleApplaba32/Smoktalni.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplaba32
+{
+    class Smoktalni
+    {
+    }
+}
